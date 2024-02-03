b623343c7fbff1d5e3e3a159f3410e4889d3d8d5
Chris <158752049+Chrismorel7@users.noreply.github.com>
2024-02-03 15:13:24 +0000

    Comment added

--- DataSet/FilterDataSet.py
+++ DataSet/FilterDataSet.py
@@ -1,4 +1,5 @@
+# Importation des bibliothèque
+## pandas --> Gerer les fichiers csv + DataScience
+## numpy --> Gerer des matrices et des tables de données
 import pandas as pd
 import numpy as np
-
-
