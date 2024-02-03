4048205e4c400861bd096c6f673fcd6e6eef0542
Chris <158752049+Chrismorel7@users.noreply.github.com>
2024-02-03 21:03:08 +0000

    Imported Pyplot

--- DataSet/FilterDataSet.py
+++ DataSet/FilterDataSet.py
@@ -1,5 +1,7 @@
 # Importation des bibliothèque
 ## pandas --> Gerer les fichiers csv + DataScience
 ## numpy --> Gerer des matrices et des tables de données
+## pyplot --> Gerer les matrices et les afficher sous forme de graphique
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
