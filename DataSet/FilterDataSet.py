4fba95a7c571cf230a5d8d9712aeb805654c7c8d
Chris <158752049+Chrismorel7@users.noreply.github.com>
2024-02-04 09:57:27 +0000

    Read csv

--- DataSet/FilterDataSet.py
+++ DataSet/FilterDataSet.py
@@ -5,3 +5,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+GH011015_acc_df = pd.read_csv('DataSet/csvFiles/GH011015_HERO9 Black-ACCL.csv')
+GH011015_gyr_df = pd.read_csv('DataSet/csvFiles/GH011015_HERO9 Black-GYRO.csv')
+GH011042_acc_df = pd.read_csv('DataSet/csvFiles/GH011042_HERO9 Black-ACCL.csv')
+GH011042_gyr_df = pd.read_csv('DataSet/csvFiles/GH011042_HERO9 Black-GYRO.csv')
