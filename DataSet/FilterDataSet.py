cf2893f32c40235a101b2d3296faa07b83574732
Chris <158752049+Chrismorel7@users.noreply.github.com>
2024-02-06 17:27:13 +0000

    Created Video1.csv

--- DataSet/FilterDataSet.py
+++ DataSet/FilterDataSet.py
@@ -33,3 +33,23 @@ Video1_Input = Video1_Input.rename(columns={"Accelerometer [m/s2]": "acc_x",
                                             "Gyroscope [rad/s]": "gyr_x",
                                             "gyr1": "gyr_y",
                                             "gyr2": "gyr_z"})
+
+Video1_Input = Video1_Input.reindex(["date", "acc_x", "acc_y", "acc_z", "gyr_x", "gyr_y", "gyr_z"], axis=1)
+
+for i in range(len(Video1_Input)):
+    if Video1_Input.loc[i]["acc_x"] <= -49 or Video1_Input.loc[i]["acc_x"] >= 49:
+        print(Video1_Input.loc[i]["acc_x"])
+        Video1_Input.loc[i,['fall']] = int(1)
+    elif Video1_Input.loc[i]["acc_y"] <= -49 or Video1_Input.loc[i]["acc_y"] >= 49:
+        print(Video1_Input.loc[i]["acc_y"])
+        Video1_Input.loc[i,['fall']] = int(1)
+    elif Video1_Input.loc[i]["acc_z"] <= -49 or Video1_Input.loc[i]["acc_z"] >= 49:
+        print(Video1_Input.loc[i]["acc_z"])
+        Video1_Input.loc[i,['fall']] = int(1)
+    else :
+        Video1_Input.loc[i,['fall']] = int(0)
+        
+print(Video1_Input.head())
+print(Video1_Input.describe())
+
+Video1_Input.to_csv('DataSet/csvFiles/Video1_Input.csv')
