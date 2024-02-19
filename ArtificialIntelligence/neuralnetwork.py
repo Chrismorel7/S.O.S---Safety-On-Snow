53f4bed1eb2baf5625432f7fb417ab2d6f6bb2fb
Chris <christophe.morel1711@gmail.com>
2024-02-19 11:45:54 +0100

    Cancelled all change

--- ArtificialIntelligence/neuralnetwork.py
+++ ArtificialIntelligence/neuralnetwork.py
@@ -20,7 +20,7 @@ class NeuralNetwork(object):
 
     def sigmoid(self, x):
         """Fonction sigmoid"""
-        return 1/(1+np.exp(-x, dtype=np.float128))
+        return 1/(1+np.exp(-x))
 
     def sigmoidprime(self, x):
         """Fonction derivée de la fonction sigmoid"""
