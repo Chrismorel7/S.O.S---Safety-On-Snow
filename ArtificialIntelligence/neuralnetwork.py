1e6652ad0090eadf5845d3de6128ad6522bf2430
Chris <158752049+Chrismorel7@users.noreply.github.com>
2024-04-03 13:10:18 +0000

    ReLu function

--- ArtificialIntelligence/neuralnetwork.py
+++ ArtificialIntelligence/neuralnetwork.py
@@ -19,58 +19,48 @@ class NeuralNetwork(object):
         self.z2_error = None
         self.z2_delta = None
 
-    def sigmoid(self, x):
-        """Fonction sigmoid"""
-        return 1/(1+np.exp(-x, dtype=np.double))
+    def ReLu(self, x):
+        """Fonction ReLu"""
+        return np.where(x > 0, x, 0)
 
-    def sigmoidprime(self, x):
-        """Fonction derivée de la fonction sigmoid"""
-        return x*(1-x)
+    def ReLuprime(self, x):
+        """Fonction derivée de la fonction ReLu"""
+        return np.where(x > 0, 1, 0)
 
     def forward(self, training_input):
-        """Fonction d'entrainement avec la méthode forward"""
-        
-        MAX_VALUE_EXP = 709
-        MIN_VALUE_EXP = -709
-        
+        """Fonction d'entrainement avec la méthode forward"""        
         self.z = np.dot(training_input, self.w1)
         
-        self.z[self.z > MAX_VALUE_EXP] = MAX_VALUE_EXP
-        self.z[self.z < MIN_VALUE_EXP] = MIN_VALUE_EXP
+        print("\nTraining_input : ", pd.DataFrame(training_input).head())
+        print(pd.DataFrame(training_input).describe())
+        print("\nW1 : ", pd.DataFrame(self.w1).head())
+        print(pd.DataFrame(self.w1).describe())
+        print("\nZ : ", pd.DataFrame(self.z).head())
+        print(pd.DataFrame(self.z).describe())
         
-        #print("\nTraining_input : ", pd.DataFrame(training_input).head())
-        #print(pd.DataFrame(training_input).describe())
-        #print("\nW1 : ", pd.DataFrame(self.w1).head())
-        #print(pd.DataFrame(self.w1).describe())
-        #print("\nZ : ", pd.DataFrame(self.z).head())
-        #print(pd.DataFrame(self.z).describe())
+        self.z2 = self.ReLu(self.z)
         
-        self.z2 = self.sigmoid(self.z)
-        
-        #print("\nZ2 : ", pd.DataFrame(self.z2).head())
-        #print(pd.DataFrame(self.z2).describe())
+        print("\nZ2 : ", pd.DataFrame(self.z2).head())
+        print(pd.DataFrame(self.z2).describe())
         
         self.z3 = np.dot(self.z2, self.w2)
+
+        print("\nZ3 : ", pd.DataFrame(self.z3).head())
+        print(pd.DataFrame(self.z3).describe())
         
-        self.z3[self.z3 > MAX_VALUE_EXP] = MAX_VALUE_EXP
-        self.z3[self.z3 < MIN_VALUE_EXP] = MIN_VALUE_EXP
+        ai_output = self.ReLu(self.z3)
         
-        #print("\nZ3 : ", pd.DataFrame(self.z3).head())
-        #print(pd.DataFrame(self.z3).describe())
-        
-        ai_output = self.sigmoid(self.z3)
-        
-        #print("\nAI Output : ", pd.DataFrame(ai_output).head())
-        #print(pd.DataFrame(ai_output).describe())
+        print("\nAI Output : ", pd.DataFrame(ai_output).head())
+        print(pd.DataFrame(ai_output).describe())
         
         return ai_output
 
     def backward(self, training_input, training_output, ai_output):
         """Fonction d'entrainement avec la méthode backward"""
         self.output_error = training_output - ai_output
-        self.output_delta = self.output_error * self.sigmoidprime(ai_output)
+        self.output_delta = self.output_error * self.ReLuprime(ai_output)
         self.z2_error = self.output_delta.dot(self.w2.T)
-        self.z2_delta = self.z2_error * self.sigmoidprime(self.z2)
+        self.z2_delta = self.z2_error * self.ReLuprime(self.z2)
         self.w1 += training_input.T.dot(self.z2_delta)
         self.w2 += self.z2.T.dot(self.output_delta)
 
