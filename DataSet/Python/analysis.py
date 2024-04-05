2b882ecabd798e68ab9c8bf0e9f2b7c188455f8f
Chris <christophe.morel1711@gmail.com>
2024-04-05 19:35:01 +0200

    Created Delta csv

--- DataSet/Python/analysis.py
+++ DataSet/Python/analysis.py
@@ -8,7 +8,7 @@ print(df1.head())
 print(df1.describe())
 
 
-fig, axs = plt.subplots(3, 1, layout='constrained')
+fig, axs = plt.subplots(2, 1, layout='constrained')
 axs[0].plot(df1.index, df1.acc_x, df1.index, df1.acc_y, df1.index, df1.acc_z)
 axs[1].plot(df1.index, df1.gyr_x, df1.index, df1.gyr_y, df1.index, df1.gyr_z)
 
@@ -16,7 +16,7 @@ axs[0].set_ylim(-200, 200)
 axs[0].set_ylabel('Accelerometer [m/s2]')
 axs[0].grid(True)
 axs[1].set_ylim(-200, 200)
-axs[1].set_ylabel('Accelerometer [m/s2]')
+axs[1].set_ylabel('Gyroscope')
 axs[1].grid(True)
 
 plt.legend(loc='best')
--- DataSet/Python/main.py
+++ DataSet/Python/main.py
@@ -2,58 +2,80 @@
 import dataset as ds
 import pandas as pd
 
-
 # ----------  Video 1  ----------
-#Dataset1 = ds.DataSet()
-#df_acc_vdo_1 = Dataset1.getdataframeacc("DataSet/csvFiles/GH011015_HERO9 Black-ACCL.csv")
-#df_gyr_vdo_1 = Dataset1.getdataframegyr("DataSet/csvFiles/GH011015_HERO9 Black-GYRO.csv")
-#Dataset1.dropcolumn('temperature [°C]', 'cts')
-#Dataset1.dropuselessvalues(7674, len(Dataset1.df_acc))
-#Dataset1.mergedataframe()
-#Dataset1.renamecolumns()
-#Dataset1.addfallingdata(49)
-#Dataset1.createcsv('DataSet/csvFiles/Input_vdo1.csv')
-#
-## ----------  Video 2  ----------
-#Dataset2 = ds.DataSet()
-#df_acc_vdo_2 = Dataset2.getdataframeacc('DataSet/csvFiles/GH011042_HERO9 Black-ACCL.csv')
-#df_gyr_vdo_2 = Dataset2.getdataframegyr('DataSet/csvFiles/GH011042_HERO9 Black-GYRO.csv')
-#Dataset2.dropcolumn('temperature [°C]', 'cts')
-#Dataset2.input = pd.merge(df_acc_vdo_2, df_gyr_vdo_2)
-#Dataset2.renamecolumns()
-#Dataset2.addfallingdata(9999)
-#Dataset2.input.to_csv('DataSet/csvFiles/Input_vdo2.csv')
-#
-## ----------  Video 3  ----------
-#Dataset3 = ds.DataSet()
-#df_acc_vdo_3 = Dataset3.getdataframeacc("DataSet/csvFiles/GX012550_HERO9 Black-ACCL.csv")
-#print(df_acc_vdo_3.head())
-#df_gyr_vdo_3 = Dataset3.getdataframegyr("DataSet/csvFiles/GX012550_HERO9 Black-GYRO.csv")
-#print(df_gyr_vdo_3.head())
-#Dataset3.dropcolumn('temperature [°C]', 'cts')
-#Dataset3.input = pd.merge(df_acc_vdo_3, df_gyr_vdo_3, on='date')
-#Dataset3.renamecolumns()
-#Dataset3.addfallingdata(9999)
-#Dataset3.input.to_csv('DataSet/csvFiles/Input_vdo3.csv')
+Dataset1 = ds.DataSet()
+df_acc_vdo_1 = Dataset1.getdataframeacc("DataSet/csvFiles/GH011015_HERO9 Black-ACCL.csv")
+df_gyr_vdo_1 = Dataset1.getdataframegyr("DataSet/csvFiles/GH011015_HERO9 Black-GYRO.csv")
+Dataset1.dropcolumn('temperature [°C]', 'cts')
+Dataset1.dropuselessvalues(7674, len(Dataset1.df_acc))
+Dataset1.mergedataframe()
+Dataset1.renamecolumns()
+Dataset1.addfallingdata(49)
+Dataset1.createcsv('DataSet/csvFiles/Input_vdo1.csv')
+print("Video 1 : DONE !")
+
+
+# ----------  Video 2  ----------
+Dataset2 = ds.DataSet()
+df_acc_vdo_2 = Dataset2.getdataframeacc('DataSet/csvFiles/GH011042_HERO9 Black-ACCL.csv')
+df_gyr_vdo_2 = Dataset2.getdataframegyr('DataSet/csvFiles/GH011042_HERO9 Black-GYRO.csv')
+Dataset2.dropcolumn('temperature [°C]', 'cts')
+Dataset2.input = pd.merge(df_acc_vdo_2, df_gyr_vdo_2)
+Dataset2.renamecolumns()
+Dataset2.addfallingdata(9999)
+Dataset2.input.to_csv('DataSet/csvFiles/Input_vdo2.csv')
+print("Video 2 : DONE !")
+
+
+# ----------  Video 3  ----------
+Dataset3 = ds.DataSet()
+df_acc_vdo_3 = Dataset3.getdataframeacc("DataSet/csvFiles/GX012550_HERO9 Black-ACCL.csv")
+print(df_acc_vdo_3.head())
+df_gyr_vdo_3 = Dataset3.getdataframegyr("DataSet/csvFiles/GX012550_HERO9 Black-GYRO.csv")
+print(df_gyr_vdo_3.head())
+Dataset3.dropcolumn('temperature [°C]', 'cts')
+Dataset3.input = pd.merge(df_acc_vdo_3, df_gyr_vdo_3, on='date')
+Dataset3.renamecolumns()
+Dataset3.addfallingdata(9999)
+Dataset3.input.to_csv('DataSet/csvFiles/Input_vdo3.csv')
+print("Video 3 : DONE !")
+
 
 # ----------  ALL  ----------
-##ds.dfcombined("DataSet/csvFiles/Input_vdo1.csv",
+#ds.dfcombined("DataSet/csvFiles/Input_vdo1.csv",
 #              "DataSet/csvFiles/Input_vdo2.csv", 
 #              "DataSet/csvFiles/Input_vdo3.csv", 
 #              "DataSet/csvFiles/Input.csv")
 #
 
-# ----------  Video 1 (delta) ----------
+## ----------  Video 1 (delta) ----------
 Dataset1_delta = ds.DataSet()
-df_acc_vdo_1 = Dataset1_delta.getdataframeacc("DataSet/csvFiles/GH011015_HERO9 Black-ACCL.csv")
-df_gyr_vdo_1 = Dataset1_delta.getdataframegyr("DataSet/csvFiles/GH011015_HERO9 Black-GYRO.csv")
-Dataset1_delta.dropcolumn('temperature [°C]', 'cts')
-Dataset1_delta.dropuselessvalues(7674, len(Dataset1_delta.df_acc))
-Dataset1_delta.mergedataframe()
-Dataset1_delta.renamecolumns()
-Dataset1_delta.input = Dataset1_delta.input.drop(axis=1, columns={'date'})
+Dataset1_delta.input = Dataset1.input.drop(axis=1, columns={'date'})
 Dataset1_delta.createdeltadf()
 print(Dataset1_delta.input)
 print(Dataset1_delta.inputdelta)
 print(Dataset1_delta.input.dtypes)
 Dataset1_delta.createdeltacsv('DataSet/csvFiles/Input_vdo1_delta.csv')
+print("Video 1 delta : DONE !")
+
+
+# ----------  Video 2 (delta) ----------
+Dataset2_delta = ds.DataSet()
+Dataset2_delta.input = Dataset2.input.drop(axis=1, columns={'date'})
+Dataset2_delta.createdeltadf()
+print(Dataset2_delta.input)
+print(Dataset2_delta.inputdelta)
+print(Dataset2_delta.input.dtypes)
+Dataset2_delta.createdeltacsv('DataSet/csvFiles/Input_vdo2_delta.csv')
+print("Video 2 delta : DONE !")
+
+
+# ----------  Video 3 (delta) ----------
+Dataset3_delta = ds.DataSet()
+Dataset3_delta.input = Dataset3.input.drop(axis=1, columns={'date'})
+Dataset3_delta.createdeltadf()
+print(Dataset3_delta.input)
+print(Dataset3_delta.inputdelta)
+print(Dataset3_delta.input.dtypes)
+Dataset3_delta.createdeltacsv('DataSet/csvFiles/Input_vdo2_delta.csv')
+print("Video 3 delta : DONE !")
