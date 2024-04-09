df87fe4d3e592aaa4917f9a23ddcb939ee888ea2
Chris <christophe.morel1711@gmail.com>
2024-04-09 19:23:11 +0200

    Combine DataFrame

--- DataSet/Python/csvcreator.py
+++ DataSet/Python/csvcreator.py
@@ -2,8 +2,8 @@ import dataset as ds
 import pandas as pd
 import numpy as np
 
-VIDEO = str(12663)
-FALLVALUELIMIT = 30
+VIDEO = str(12548)
+FALLVALUELIMIT = 40
 
 Dataset = ds.DataSet()
 df_acc = Dataset.getdataframeacc("DataSet/csvFiles/Originals/GX0"+VIDEO+"_HERO9 Black-ACCL.csv")
