a3d54f3858011a1f9a442c1475f2449a7818a301
Chris <christophe.morel1711@gmail.com>
2024-04-09 18:43:38 +0200

    12549

--- DataSet/Python/analysis.py
+++ DataSet/Python/analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-VIDEO = str(12548)
+VIDEO = str(12549)
 
 dfplot = pd.read_csv('DataSet/csvFiles/csvDelta/Input_vdo'+VIDEO+'_delta.csv')
 print("\n\nDataframe :\n")
--- DataSet/Python/temp.py
+++ DataSet/Python/temp.py
@@ -2,8 +2,8 @@ import dataset as ds
 import pandas as pd
 import numpy as np
 
-VIDEO = str(12548)
-FALLVALUELIMIT = 30
+VIDEO = str(12549)
+FALLVALUELIMIT = 60
 
 Dataset = ds.DataSet()
 df_acc = Dataset.getdataframeacc("DataSet/csvFiles/Originals/GX0"+VIDEO+"_HERO9 Black-ACCL.csv")
