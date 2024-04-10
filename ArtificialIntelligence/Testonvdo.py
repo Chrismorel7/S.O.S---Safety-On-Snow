97b5d7cb8d4c6338c982a463f74e4b12866e3a21
Chris <christophe.morel1711@gmail.com>
2024-04-10 13:11:24 +0200

    Useless_values deleted

--- ArtificialIntelligence/Testonvdo.py
+++ ArtificialIntelligence/Testonvdo.py
@@ -6,11 +6,14 @@ output = pd.DataFrame()
 input = pd.read_csv('DataSet/csvFiles/csvDelta/Input_vdo11015_delta.csv')
 input = input.drop(axis=1, columns={"oldindex", "fall"})
 
-NN = nn.NeuralNetwork()
+NN = nn.NeuralNetwork(input_size=1)
 TrainMaxSeries = np.loadtxt("inputmax.txt")
 TrainMinSeries = np.loadtxt("inputmin.txt")
 
-output = NN.forward(input)
-outputfall = output[output['fall'] == 1.0]
+input = (input - TrainMinSeries) / (TrainMaxSeries - TrainMinSeries)
+
+output = pd.read_csv("test.csv")
+print(output.describe())
+outputfall = output[output["fall"] == 1.0]
 
 print(outputfall)
