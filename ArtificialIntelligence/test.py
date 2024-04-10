67957912e205ebf3140d7a1df826ec53607ae147
Chris <christophe.morel1711@gmail.com>
2024-04-10 13:40:46 +0200

    Updated test.py for better code understanding

--- ArtificialIntelligence/test.py
+++ ArtificialIntelligence/test.py
@@ -5,7 +5,12 @@ neuralnetwork = nn.NeuralNetwork(input_size=0)
 TrainMaxSeries = np.loadtxt("inputmax.txt")
 TrainMinSeries = np.loadtxt("inputmin.txt")
 
-PredictSeries = np.abs([-42.37649880095923,88.58033573141486,28.06235011990408,1.887113951011714,-0.2939297124600646,-12.562300319488818])
+PredictSeries = np.abs([-42.37649880095923,
+                        88.58033573141486,
+                        28.06235011990408,
+                        1.887113951011714,
+                        -0.2939297124600646,
+                        -12.562300319488818])
 PredictSeries = (PredictSeries - TrainMinSeries) / (TrainMaxSeries - TrainMinSeries)
 
 neuralnetwork.predict(PredictSeries)
